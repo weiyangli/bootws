542a0a829d977fc02fa9ca1a0c94ca7e99903a15
lwy <15754630037@163.com>
2019-11-25 15:36:43 +0800

    添加单元测试

--- boot-ws/src/test/java/com/lwy/bootws/BootWsApplicationTests.java
+++ boot-ws/src/test/java/com/lwy/bootws/BootWsApplicationTests.java
@@ -1,13 +1,22 @@
 package com.lwy.bootws;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {
+        PropertySourcesPlaceholderConfigurer.class
+})
 @SpringBootTest
 class BootWsApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        System.out.println("fdsf");
     }
 
 }
